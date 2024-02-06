b46063d7c04d658743add85767083c574ffef499
Zhenyu Yang <84153409+atzhenyuyang@users.noreply.github.com>
2024-02-06 22:50:38 +0800

    Add files via upload
    Pocket_StrMod code

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .utils import *
+from .autoregressive_process import *
+from .autclass import Generate
---
+++ autoregressive_process/edge.py
@@ -0,0 +1,177 @@
+import torch
+from torch import nn
+from easydict import EasyDict
+import torch.nn.functional as F
+from .net_utils import AtomEmbedding, embed_compose
+from .position_predictor import PositionPredictor
+from .pivotal_atom_selector import pivotalLayerVN
+from .new_atom_generator import AtomFlow
+from .chemical_bond_generator import BondFlow
+from .context_encoder import ContextEncoder
+
+
+encoder_cfg = EasyDict(
+    {'edge_channels':8, 'num_interactions':6,
+     'knn':32, 'cutoff':10.0}
+     )
+pivotal_net_cfg = EasyDict(
+    {'hidden_dim_sca':32, 'hidden_dim_vec':8}
+    )
+atom_flow_cfg = EasyDict(
+    {'hidden_dim_sca':32, 'hidden_dim_vec':8, 'num_lig_atom_type':10,
+     'num_flow_layers':6}
+    )
+pos_predictor_cfg = EasyDict(
+    {'num_filters':[64,64], 'n_component':3}
+    )
+pos_filter_cfg = EasyDict(
+    {'edge_channels':8, 'num_filters':[32,16]}
+    )
+edge_flow_cfg = EasyDict(
+    {'edge_channels':8, 'num_filters':[32,8], 'num_bond_types':3,
+     'num_heads':2, 'cutoff':10.0, 'num_st_layers':3}
+    )
+config = EasyDict(
+    {'deq_coeff':0.9, 'hidden_channels':32, 'hidden_channels_vec':8, 'bottleneck':8, 'use_conv1d':False,
+     'encoder':encoder_cfg, 'atom_flow':atom_flow_cfg, 'pos_predictor':pos_predictor_cfg,
+     'pos_filter':pos_filter_cfg, 'edge_flow':edge_flow_cfg, 'pivotal_net':pivotal_net_cfg}
+    )
+
+
+class pocket_strmod_WithEdge(nn.Module):
+    def __init__(self, config) -> None:
+        super(pocket_strmod_WithEdge, self).__init__()
+        self.config = config
+        self.num_bond_types = config.num_bond_types
+        
+        self.emb_dim = [config.hidden_channels, config.hidden_channels_vec]
+        self.protein_atom_emb = AtomEmbedding(config.protein_atom_feature_dim, 1, *self.emb_dim)
+        self.ligand_atom_emb = AtomEmbedding(config.ligand_atom_feature_dim, 1, *self.emb_dim)
+        self.atom_type_embedding = nn.Embedding(config.num_atom_type, config.hidden_channels)
+
+        self.encoder = ContextEncoder(
+            hidden_channels=self.emb_dim, edge_channels=config.encoder.edge_channels, 
+            num_edge_types=config.num_bond_types, num_interactions=config.encoder.num_interactions, 
+            k=config.encoder.knn, cutoff=config.encoder.cutoff, bottleneck=config.bottleneck,
+            use_conv1d=config.use_conv1d
+            )
+        self.pivotal_net = pivotalLayerVN(
+            self.emb_dim[0], self.emb_dim[1], config.pivotal_net.hidden_dim_sca, 
+            config.pivotal_net.hidden_dim_vec, bottleneck=config.bottleneck,
+            use_conv1d=config.use_conv1d
+            )
+        self.atom_flow = AtomFlow(
+            self.emb_dim[0], self.emb_dim[1], config.atom_flow.hidden_dim_sca,
+            config.atom_flow.hidden_dim_vec, num_lig_atom_type=config.atom_flow.num_lig_atom_type,
+            num_flow_layers=config.atom_flow.num_flow_layers, bottleneck=config.bottleneck,
+            use_conv1d=config.use_conv1d
+            )
+        self.pos_predictor = PositionPredictor(
+            self.emb_dim[0], self.emb_dim[1], config.pos_predictor.num_filters,
+            config.pos_predictor.n_component, bottleneck=config.bottleneck,
+            use_conv1d=config.use_conv1d
+            )
+        self.edge_flow = BondFlow(
+            self.emb_dim[0], self.emb_dim[1], config.edge_flow.edge_channels, 
+            config.edge_flow.num_filters, config.edge_flow.num_bond_types, 
+            num_heads=config.edge_flow.num_heads, cutoff=config.edge_flow.cutoff,
+            num_st_layers=config.edge_flow.num_st_layers, bottleneck=config.bottleneck,
+            use_conv1d=config.use_conv1d
+            )
+    
+    def get_parameter_number(self):                                                                                                                                                                                     
+        total_num = sum(p.numel() for p in self.parameters())
+        trainable_num = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        return {'Total': total_num, 'Trainable': trainable_num}
+
+    def get_loss(self, data, annealing=False):
+        h_cpx = embed_compose(data.cpx_feature.float(), data.cpx_pos, data.idx_ligand_ctx_in_cpx, 
+                              data.idx_protein_in_cpx, self.ligand_atom_emb, 
+                              self.protein_atom_emb, self.emb_dim)
+        h_cpx = self.encoder(
+            node_attr = h_cpx,
+            pos = data.cpx_pos,
+            edge_index = data.cpx_edge_index,
+            edge_feature = data.cpx_edge_feature,
+            annealing=annealing
+        )
+        pivotal_pred = self.pivotal_net(h_cpx, data.idx_ligand_ctx_in_cpx)
+        pivotal_loss = F.binary_cross_entropy_with_logits(
+            input=pivotal_pred, target=data.ligand_pivotal.view(-1, 1).float()
+        )
+        pivotal_pred_apo = self.pivotal_net(h_cpx, data.apo_protein_idx)
+        surf_loss = F.binary_cross_entropy_with_logits(
+            input=pivotal_pred_apo, target=data.candidate_pivotal_label_in_protein.view(-1, 1).float()
+        )
+        x_z = F.one_hot(data.atom_label, num_classes=self.config.atom_flow.num_lig_atom_type).float()  
+        x_z += self.config.deq_coeff * torch.rand(x_z.size(), device=x_z.device) 
+        z_atom, atom_log_jacob = self.atom_flow(x_z, h_cpx, data.pivotal_idx_in_context)
+        ll_atom = (1/2 * (z_atom ** 2) - atom_log_jacob).mean() 
+
+        atom_type_emb = self.atom_type_embedding(data.atom_label)
+        relative_mu, abs_mu, sigma, pi = self.pos_predictor(
+            h_cpx,  
+            data.pivotal_idx_in_context,
+            data.cpx_pos, 
+            atom_type_emb=atom_type_emb,
+            )
+        loss_pos = -torch.log(
+            self.pos_predictor.get_mdn_probability(abs_mu, sigma, pi, data.y_pos) + 1e-16
+            ).mean()
+        
+        z_edge = F.one_hot(data.edge_label, num_classes=4).float()  
+        z_edge += self.config.deq_coeff * torch.rand(z_edge.size(), device=z_edge.device) 
+        edge_index_query = torch.stack([data.edge_query_index_0, data.edge_query_index_1])
+        pos_query_knn_edge_idx = torch.stack(
+            [data.pos_query_knn_edge_idx_0, data.pos_query_knn_edge_idx_1]
+            )
+        z_edge, edge_log_jacob = self.edge_flow(
+            z_edge=z_edge,
+            pos_query=data.y_pos, 
+            edge_index_query=edge_index_query, 
+            cpx_pos=data.cpx_pos, 
+            node_attr_compose=h_cpx, 
+            edge_index_q_cps_knn=pos_query_knn_edge_idx, 
+            index_real_cps_edge_for_atten=data.index_real_cps_edge_for_atten, 
+            tri_edge_index=data.tri_edge_index, 
+            tri_edge_feat=data.tri_edge_feat,
+            atom_type_emb=atom_type_emb,
+            annealing=annealing
+            )
+        ll_edge = (1/2 * (z_edge ** 2) - edge_log_jacob).mean()
+        
+        pos_fake_knn_edge_idx = torch.stack([data.pos_fake_knn_edge_idx_0, data.pos_fake_knn_edge_idx_1])
+        pos_fake_pred, _ = self.edge_flow.pos_classfier(
+            pos_query=data.pos_fake, 
+            edge_index_q_cps_knn=pos_fake_knn_edge_idx,
+            cpx_pos=data.cpx_pos,
+            node_attr_compose=h_cpx,
+            annealing=annealing
+            )
+        loss_fake = F.binary_cross_entropy_with_logits(
+            input=pos_fake_pred, target=torch.zeros_like(pos_fake_pred)
+        )
+        pos_real_knn_edge_idx = torch.stack([data.pos_real_knn_edge_idx_0, data.pos_real_knn_edge_idx_1])
+        pos_real_pred, _ = self.edge_flow.pos_classfier(
+            pos_query=data.pos_real,
+            edge_index_q_cps_knn=pos_real_knn_edge_idx,
+            cpx_pos=data.cpx_pos,
+            node_attr_compose=h_cpx,
+            annealing=annealing
+            )
+        loss_real = F.binary_cross_entropy_with_logits(
+            input=pos_real_pred, target=torch.ones_like(pos_real_pred)
+        )
+        
+        loss = torch.nan_to_num(ll_atom)\
+             + torch.nan_to_num(loss_pos)\
+             + torch.nan_to_num(ll_edge)\
+             + torch.nan_to_num(pivotal_loss)\
+             + torch.nan_to_num(loss_fake)\
+             + torch.nan_to_num(loss_real)\
+             + torch.nan_to_num(surf_loss)
+        out_dict = {
+            'loss':loss, 'loss_atom':ll_atom, 'loss_edge':ll_edge, 'loss_fake':loss_fake, 'loss_real':loss_real,
+            'loss_pos':loss_pos, 'pivotal_loss':pivotal_loss, 'surf_loss':torch.nan_to_num(surf_loss)
+            }
+        return out_dict
